9f90a31f1f8f0201bfa46f9aa8a2c5651890ccd5
Jake Mathews <mathewsj2@wit.edu>
2017-07-12 10:38:43 -0400

    Adding CRAN to Forge

--- src/main/java/com/blackducksoftware/integration/hub/bdio/simple/model/Forge.java
+++ src/main/java/com/blackducksoftware/integration/hub/bdio/simple/model/Forge.java
@@ -49,6 +49,8 @@ public class Forge {
 
     public static final Forge RUBYGEMS = new Forge("rubygems", "=");
 
+    public static final Forge CRAN = new Forge("cran", "/");
+
     private final String name;
 
     private final String separator;
