04ca4b69468e785220d81007489afce5e1a591b5
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-09-26 18:03:52 -0400

    Adding transforming from bdio to dependency node.

---
+++ src/main/java/com/blackducksoftware/integration/hub/bdio/simple/BdioTransformer.java
@@ -0,0 +1,108 @@
+/**
+ * Integration Bdio
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.bdio.simple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioComponent;
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioRelationship;
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode;
+import com.blackducksoftware.integration.hub.bdio.simple.model.Forge;
+import com.blackducksoftware.integration.hub.bdio.simple.model.SimpleBdioDocument;
+import com.blackducksoftware.integration.hub.bdio.simple.model.externalid.ExternalId;
+
+public class BdioTransformer {
+    private final Map<String, Forge> forgeMap;
+
+    public BdioTransformer(final Map<String, Forge> forgeMap) {
+        this.forgeMap = forgeMap;
+    }
+
+    public DependencyNode transformToDependencyNode(final SimpleBdioDocument document) {
+        // returns a dependency node that is the project
+
+        final List<BdioComponent> components = new ArrayList<>(document.components);
+        components.add(document.project);
+
+        final Map<String, DependencyNode> map = componentsToNodes(components);
+
+        return map.get(document.project.id);
+
+    }
+
+    public Map<String, DependencyNode> componentsToNodes(final List<BdioComponent> components) {
+        final Map<String, DependencyNode> map = new HashMap<>();
+
+        for (final BdioComponent component : components) {
+            final Forge forge = forgeMap.get(component.bdioExternalIdentifier.forge);
+            final ExternalId id = uncreateExternalId(forge, component.bdioExternalIdentifier.externalId, component.name, component.version);
+            final DependencyNode node = new DependencyNode(component.name, component.version, id);
+            map.put(component.id, node);
+        }
+
+        for (final BdioComponent component : components) {
+            final DependencyNode node = map.get(component.id);
+            for (final BdioRelationship relation : component.relationships) {
+                node.children.add(map.get(relation.related));
+            }
+        }
+
+        return map;
+    }
+
+    public ExternalId uncreateExternalId(final Forge forge, final String fullExternalId, final String name, final String revision) {
+        final String[] pieces = StringUtils.split(fullExternalId, forge.getSeparator());
+        final ExternalId id = new ExternalId(forge);
+
+        if (pieces.length == 1) {
+            // assume path? could be a 1 length module id?
+            id.path = pieces[0];
+        } else if (pieces.length == 2 || pieces.length == 3) {
+            if (pieces[0].equals(name)) {
+                id.name = pieces[0];
+                id.version = pieces[1];
+                if (pieces.length > 2) {
+                    id.architecture = pieces[2];
+                }
+            } else if (pieces[1].equals(name) && pieces[2].equals(revision)) {
+                id.group = pieces[0];
+                id.name = pieces[1];
+                id.version = pieces[2];
+            } else {
+                id.moduleNames = pieces;
+            }
+
+        } else {
+            id.moduleNames = pieces;
+        }
+
+        return id;
+    }
+
+}
---
+++ src/test/java/com/blackducksoftware/integration/hub/bdio/simple/BdioTransformerTest.java
@@ -0,0 +1,116 @@
+/**
+ * Integration Bdio
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.bdio.simple;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.json.JSONException;
+import org.junit.Test;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioComponent;
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioRelationship;
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode;
+import com.blackducksoftware.integration.hub.bdio.simple.model.Forge;
+import com.blackducksoftware.integration.hub.bdio.simple.model.SimpleBdioDocument;
+import com.google.gson.Gson;
+
+public class BdioTransformerTest {
+    private final JsonTestUtils jsonTestUtils = new JsonTestUtils();
+
+    @Test
+    public void testTransformingDependencyNodes() throws URISyntaxException, IOException, JSONException {
+        final String expectedJson = jsonTestUtils.getExpectedJson("sample.jsonld");
+
+        final Reader reader = new StringReader(expectedJson);
+        SimpleBdioDocument doc = null;
+        try (BdioReader bdioReader = new BdioReader(new Gson(), reader)) {
+            doc = bdioReader.readSimpleBdioDocument();
+        }
+
+        final Map<String, Forge> forgeMap = new HashMap<>();
+        forgeMap.put("maven", Forge.MAVEN);
+        final BdioTransformer transformer = new BdioTransformer(forgeMap);
+
+        final DependencyNode node = transformer.transformToDependencyNode(doc);
+
+        assertEquals("gradleTestProject", node.name);
+
+        final BdioPropertyHelper bdioPropertyHelper = new BdioPropertyHelper();
+        final BdioNodeFactory bdioNodeFactory = new BdioNodeFactory(bdioPropertyHelper);
+        final DependencyNodeTransformer dependencyNodeTransformer = new DependencyNodeTransformer(bdioNodeFactory, bdioPropertyHelper);
+        final SimpleBdioDocument simpleBdioDocument = dependencyNodeTransformer.transformDependencyNode(node);
+
+        simpleBdioDocument.billOfMaterials.id = doc.billOfMaterials.id;
+        simpleBdioDocument.billOfMaterials.customData = doc.billOfMaterials.customData;
+
+        assertEquals(true, EqualsBuilder.reflectionEquals(simpleBdioDocument.billOfMaterials, doc.billOfMaterials));
+        assertEquals(true, EqualsBuilder.reflectionEquals(simpleBdioDocument.project, doc.project, "bdioExternalIdentifier", "relationships"));
+        assertEquals(true, EqualsBuilder.reflectionEquals(simpleBdioDocument.project.bdioExternalIdentifier, doc.project.bdioExternalIdentifier));
+        assertRelationships(doc.project.relationships, simpleBdioDocument.project.relationships);
+
+        assertEquals(doc.components.size(), simpleBdioDocument.components.size());
+        for (final BdioComponent expected : simpleBdioDocument.components) {
+            boolean fnd = false;
+            for (final BdioComponent actual : doc.components) {
+                if (expected.id.equals(actual.id)) {
+                    assertEquals(false, fnd);
+                    fnd = true;
+
+                    assertEquals(true, EqualsBuilder.reflectionEquals(expected, actual, "bdioExternalIdentifier", "relationships"));
+                    assertEquals(true, EqualsBuilder.reflectionEquals(expected.bdioExternalIdentifier, actual.bdioExternalIdentifier));
+                    assertRelationships(expected.relationships, actual.relationships);
+
+                }
+            }
+            assertEquals(expected.id, true, fnd);
+        }
+
+    }
+
+    private void assertRelationships(final List<BdioRelationship> expectedList, final List<BdioRelationship> actualList) {
+        assertEquals(expectedList.size(), actualList.size());
+        for (final BdioRelationship expected : expectedList) {
+            boolean fnd = false;
+            for (final BdioRelationship actual : actualList) {
+                if (expected.related.equals(actual.related)) {
+                    assertEquals(false, fnd);
+                    fnd = true;
+
+                    assertEquals(true, EqualsBuilder.reflectionEquals(expected, actual));
+                }
+            }
+            assertEquals(expected.related, true, fnd);
+        }
+    }
+
+}
