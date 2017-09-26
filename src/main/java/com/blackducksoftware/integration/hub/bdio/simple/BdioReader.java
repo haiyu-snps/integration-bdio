e30ef5235b6ce1f2391ffb6775196bf54a5c73eb
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-09-26 16:27:05 -0400

    Adding bdio reader with tests.

---
+++ src/main/java/com/blackducksoftware/integration/hub/bdio/simple/BdioReader.java
@@ -0,0 +1,85 @@
+/**
+ * Integration Bdio
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.bdio.simple;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioBillOfMaterials;
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioComponent;
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioProject;
+import com.blackducksoftware.integration.hub.bdio.simple.model.SimpleBdioDocument;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class BdioReader implements Closeable {
+    private final Gson gson;
+
+    private final JsonReader jsonReader;
+
+    public BdioReader(final Gson gson, final Reader reader) throws IOException {
+        this.gson = gson;
+        this.jsonReader = new JsonReader(reader);
+        jsonReader.beginArray();
+    }
+
+    public BdioReader(final Gson gson, final InputStream inputStream) throws IOException {
+        this.gson = gson;
+        this.jsonReader = new JsonReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        jsonReader.beginArray();
+    }
+
+    public SimpleBdioDocument readSimpleBdioDocument() throws IOException {
+        final SimpleBdioDocument document = new SimpleBdioDocument();
+        document.billOfMaterials = readBillOfMaterials();
+        document.project = readProject();
+        while (jsonReader.hasNext()) {
+            document.components.add(readComponent());
+        }
+        return document;
+    }
+
+    public BdioBillOfMaterials readBillOfMaterials() {
+        return gson.fromJson(jsonReader, BdioBillOfMaterials.class);
+    }
+
+    public BdioProject readProject() {
+        return gson.fromJson(jsonReader, BdioProject.class);
+    }
+
+    public BdioComponent readComponent() {
+        return gson.fromJson(jsonReader, BdioComponent.class);
+    }
+
+    @Override
+    public void close() throws IOException {
+        jsonReader.endArray();
+        jsonReader.close();
+    }
+
+}
---
+++ src/test/java/com/blackducksoftware/integration/hub/bdio/simple/BdioReaderTest.java
@@ -0,0 +1,105 @@
+/**
+ * Integration Bdio
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.bdio.simple;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+
+import org.json.JSONException;
+import org.junit.Test;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.BdioComponent;
+import com.blackducksoftware.integration.hub.bdio.simple.model.SimpleBdioDocument;
+import com.google.gson.Gson;
+
+public class BdioReaderTest {
+    private final JsonTestUtils jsonTestUtils = new JsonTestUtils();
+
+    @Test
+    public void testReaderOnSample() throws FileNotFoundException, IOException, URISyntaxException, JSONException {
+        final String expectedJson = jsonTestUtils.getExpectedJson("sample.jsonld");
+
+        final Reader reader = new StringReader(expectedJson);
+        SimpleBdioDocument doc = null;
+        try (BdioReader bdioReader = new BdioReader(new Gson(), reader)) {
+            doc = bdioReader.readSimpleBdioDocument();
+        }
+
+        assertNotNull(doc);
+
+        assertNotNull(doc.billOfMaterials);
+        assertNotNull(doc.billOfMaterials.relationships);
+        assertEquals("1.1.0", doc.billOfMaterials.bdioSpecificationVersion);
+        assertEquals("uuid:45772d33-5353-44f1-8681-3d8a15540646", doc.billOfMaterials.id);
+        assertEquals("BillOfMaterials", doc.billOfMaterials.type);
+        assertEquals("1.2.3-SNAPSHOT", doc.billOfMaterials.customData.get("testVersion"));
+        assertEquals("gradleTestProject/99.5-SNAPSHOT Black Duck I/O Export", doc.billOfMaterials.spdxName);
+        assertEquals(0, doc.billOfMaterials.relationships.size());
+
+        assertNotNull(doc.project);
+        assertNotNull(doc.project.bdioExternalIdentifier);
+        assertNotNull(doc.project.relationships);
+        assertEquals("99.5-SNAPSHOT", doc.project.version);
+        assertEquals("http:maven/com_blackducksoftware_gradle_test/gradleTestProject/99_5_SNAPSHOT", doc.project.id);
+        assertEquals("Project", doc.project.type);
+        assertEquals("gradleTestProject", doc.project.name);
+        assertEquals("maven", doc.project.bdioExternalIdentifier.forge);
+        assertEquals("com.blackducksoftware.gradle.test:gradleTestProject:99.5-SNAPSHOT", doc.project.bdioExternalIdentifier.externalId);
+
+        assertEquals(1, doc.project.relationships.size());
+        assertEquals("http:maven/org_apache_cxf/cxf_bundle/2_7_7", doc.project.relationships.get(0).related);
+        assertEquals("DYNAMIC_LINK", doc.project.relationships.get(0).relationshipType);
+
+        assertNotNull(doc.components);
+        assertEquals(4, doc.components.size());
+
+        final BdioComponent first = doc.components.get(0);
+
+        assertNotNull(first);
+        assertNotNull(first.bdioExternalIdentifier);
+        assertNotNull(first.relationships);
+
+        assertEquals("cxf-bundle", first.name);
+        assertEquals("2.7.7", first.version);
+        assertEquals("http:maven/org_apache_cxf/cxf_bundle/2_7_7", first.id);
+        assertEquals("Component", first.type);
+
+        assertEquals("maven", first.bdioExternalIdentifier.forge);
+        assertEquals("org.apache.cxf:cxf-bundle:2.7.7", first.bdioExternalIdentifier.externalId);
+
+        assertEquals(2, first.relationships.size());
+        assertEquals("http:maven/org_apache_velocity/velocity/1_7", first.relationships.get(0).related);
+        assertEquals("DYNAMIC_LINK", first.relationships.get(0).relationshipType);
+        assertEquals("http:maven/commons_lang/commons_lang/2_6", first.relationships.get(1).related);
+        assertEquals("DYNAMIC_LINK", first.relationships.get(1).relationshipType);
+
+    }
+
+}
