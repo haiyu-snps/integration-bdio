98a0b858d08343f1e0111d408f07ca78aa5e319c
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-11-29 14:02:45 -0500

    adding new forges with separators

--- src/main/java/com/blackducksoftware/integration/hub/bdio/model/Forge.java
+++ src/main/java/com/blackducksoftware/integration/hub/bdio/model/Forge.java
@@ -32,35 +32,67 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 public class Forge {
     public static final Map<String, Forge> FORGE_NAME_TO_FORGE = new HashMap<>();
 
-    public static final Forge ALPINE = new Forge("alpine", "/");
-    public static final Forge ANACONDA = new Forge("anaconda", "=");
-    public static final Forge BOWER = new Forge("bower", "#");
-    public static final Forge BUSYBOX = new Forge("busybox", "/");
-    public static final Forge CENTOS = new Forge("centos", "/");
-    public static final Forge COCOAPODS = new Forge("cocoapods", ":");
-    public static final Forge CPAN = new Forge("cpan", "-");
-    public static final Forge CRAN = new Forge("cran", "/");
-    public static final Forge DEBIAN = new Forge("debian", "/");
-    public static final Forge FEDORA = new Forge("fedora", "/");
-    public static final Forge GOGET = new Forge("goget", "");
-    public static final Forge GOLANG = new Forge("golang", ":");
-    public static final Forge HEX = new Forge("hex", "/");
-    public static final Forge MAVEN = new Forge("maven", ":");
-    public static final Forge NPM = new Forge("npm", "@");
-    public static final Forge NUGET = new Forge("nuget", "/");
-    public static final Forge PACKAGIST = new Forge("packagist", ":");
-    public static final Forge PEAR = new Forge("pear", "/");
-    public static final Forge PYPI = new Forge("pypi", "/");
-    public static final Forge REDHAT = new Forge("redhat", "/");
-    public static final Forge RUBYGEMS = new Forge("rubygems", "=");
-    public static final Forge UBUNTU = new Forge("ubuntu", "/");
+    /**
+     * The various separators here are to support three systems 1) bdio, 2) Black Duck Hub, 3) Black Duck Knowledge Base.
+     *
+     * To support matching in various Hub versions, the separators need to conform to the separators defined in the source of the bdio project here:
+     * https://github.com/blackducksoftware/bdio/blob/2.1.1/bdio/src/main/java/com/blackducksoftware/bdio/model/ExternalIdentifierBuilder.java
+     *
+     * However, as of Hub 4.3.2, those separators are internally transformed to the KB separators, which are (currently) defined here: https://confluence.dc1.lan/display/KB/KB+Details+Service#KBDetailsService-ForgeNames
+     *
+     * As of 2017-11-29, only forward slashes and colons are used for KB separators.
+     */
+    public static final Forge ALPINE = new Forge("alpine", "/", "/");
+    public static final Forge ANACONDA = new Forge("anaconda", "=", "/");
+    public static final Forge ANDROID = new Forge("android", ":", ":");
+    public static final Forge APACHE_SOFTWARE = new Forge("apache_software", "/", "/");
+    public static final Forge BITBUCKET = new Forge("bitbucket", "/", "/");
+    public static final Forge BOWER = new Forge("bower", "#", "/");
+    public static final Forge BUSYBOX = new Forge("busybox", "/", "/");
+    public static final Forge CENTOS = new Forge("centos", "/", "/");
+    public static final Forge COCOAPODS = new Forge("cocoapods", ":", ":");
+    public static final Forge CODEPLEX = new Forge("codeplex", "/", "/");
+    public static final Forge CODEPLEX_GROUP = new Forge("codeplex_group", "/", "/");
+    public static final Forge CPAN = new Forge("cpan", "-", "/");
+    public static final Forge CPE = new Forge("cpe", ":", ":");
+    public static final Forge CRAN = new Forge("cran", "/", "/");
+    public static final Forge DEBIAN = new Forge("debian", "/", "/");
+    public static final Forge FEDORA = new Forge("fedora", "/", "/");
+    public static final Forge FREEDESKTOP_ORG = new Forge("freedesktop_org", "/", "/");
+    public static final Forge GITCAFE = new Forge("gitcafe", "/", "/");
+    public static final Forge GITHUB = new Forge("github", ":", ":");
+    public static final Forge GITLAB = new Forge("gitlab", "/", "/");
+    public static final Forge GITORIOUS = new Forge("gitorious", "/", "/");
+    public static final Forge GNU = new Forge("gnu", "/", "/");
+    public static final Forge GOGET = new Forge("goget", "", "/");
+    public static final Forge GOLANG = new Forge("golang", ":", ":");
+    public static final Forge GOOGLECODE = new Forge("googlecode", "/", "/");
+    public static final Forge HEX = new Forge("hex", "/", "/");
+    public static final Forge JAVA_NET = new Forge("java_net", "/", "/");
+    public static final Forge KDE_ORG = new Forge("kde_org", "/", "/");
+    public static final Forge LAUNCHPAD = new Forge("launchpad", "/", "/");
+    public static final Forge LONG_TAIL = new Forge("long_tail", "/", "/");
+    public static final Forge MAVEN = new Forge("maven", ":", ":");
+    public static final Forge NPM = new Forge("npm", "@", "/");
+    public static final Forge NUGET = new Forge("nuget", "/", "/");
+    public static final Forge PACKAGIST = new Forge("packagist", ":", ":");
+    public static final Forge PEAR = new Forge("pear", "/", "/");
+    public static final Forge PYPI = new Forge("pypi", "/", "/");
+    public static final Forge REDHAT = new Forge("redhat", "/", "/");
+    public static final Forge RUBYFORGE = new Forge("rubyforge", "/", "/");
+    public static final Forge RUBYGEMS = new Forge("rubygems", "=", "/");
+    public static final Forge SOURCEFORGE = new Forge("sourceforge", "/", "/");
+    public static final Forge SOURCEFORGE_JP = new Forge("sourceforge_jp", "/", "/");
+    public static final Forge UBUNTU = new Forge("ubuntu", "/", "/");
 
     private final String name;
     private final String separator;
+    private final String kbSeparator;
 
-    public Forge(final String name, final String separator) {
+    public Forge(final String name, final String separator, final String kbSeparator) {
         this.name = name;
         this.separator = separator;
+        this.kbSeparator = kbSeparator;
         FORGE_NAME_TO_FORGE.put(name, this);
     }
 
@@ -85,6 +117,10 @@ public class Forge {
 
     public String getSeparator() {
         return separator;
+    }
+
+    public String getKbSeparator() {
+        return kbSeparator;
     }
 
 }
